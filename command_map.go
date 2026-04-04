4126273691bb3866bfb4a657c6f4574aea882514
kobold-king <nick.ragona@gmail.com>
2026-04-03 21:22:45 -0600

    edits

--- command_map.go
+++ command_map.go
@@ -29,7 +29,7 @@ func commandMapb(cfg *config) error {
 	if err != nil {
 		return err
 	}
-	//set the next and prev in cfg from daa recieved from jsonn
+	//set the next and prev in cfg from daa recieved from jsonnn
 	cfg.nextURL = locationResp.Next
 	cfg.prevURL = locationResp.Previous
 
