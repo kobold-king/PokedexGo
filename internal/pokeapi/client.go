002940e48df077254707b78939ffc8d1a99b5975
kobold-king <nick.ragona@gmail.com>
2026-04-05 20:47:38 -0600

    pokecache

--- internal/pokeapi/client.go
+++ internal/pokeapi/client.go
@@ -7,12 +7,14 @@ import (
 
 // Client -
 type Client struct {
+	cache      pokecache.Cache
 	httpClient http.Client
 }
 
 // NewClient -
-func NewClient(timeout time.Duration) Client {
+func NewClient(timeout, cacheInterval time.Duration) Client {
 	return Client{
+		cache: pokecache.NewCache(cacheInterval),
 		httpClient: http.Client{
 			Timeout: timeout,
 		},
